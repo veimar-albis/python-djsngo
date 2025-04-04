02b15b3e5498b07068624e0160d3488b9cc438e2
veimar-albis <veimar.albis@gmail.com>
2025-04-04 16:16:25 -0400

    avance ejercicios listas y tuplas

---
+++ ejercicios_listas_tuplas.py
@@ -0,0 +1,14 @@
+# Ejercicio 1
+'''cantidad=int(input("Ingrese la cantidad de materias a registrar: "))
+materia = []
+for i in range(cantidad):
+    materia.append(input("Ingrese el nombre de la materia: "))
+print("La lista de materias es: ", materia)'''
+# Ejercicio 2
+cantidad=int(input("Ingrese la cantidad de materias a registrar: "))
+materias = []
+for i in range(cantidad):
+    materias.append(input("Ingrese el nombre de la materia: "))
+for materia in materias:
+    print("yo estudio: ",materia)
+# Ejercicio 3
