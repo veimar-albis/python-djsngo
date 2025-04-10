fa568e9cfac6c9808751029f1681175eef855ec7
veimar-albis <veimar.albis@gmail.com>
2025-04-10 12:54:26 -0400

    avance ejercicios diccioarios 04-10-25

--- ejercicios_diccionarios.py
+++ ejercicios_diccionarios.py
@@ -57,11 +57,41 @@ else:
     print('El mes no es correcto')'''
 #ejercicio5
 '''Escribir un programa que almacene el diccionario con los créditos de las asignaturas de un curso {'Matemáticas': 6, 'Física': 4, 'Química': 5} y después muestre por pantalla los créditos de cada asignatura en el formato <asignatura> tiene <créditos> créditos, donde <asignatura> es cada una de las asignaturas del curso, y <créditos> son sus créditos. Al final debe mostrar también el número total de créditos del curso.'''
-asignaturas = {'Matemáticas': 6, 'Física': 4, 'Química': 5}
+'''asignaturas = {'Matemáticas': 6, 'Física': 4, 'Química': 5}
 total_creditos = 0
 for asignatura, creditos in asignaturas.items():
     print(f'{asignatura} tiene {creditos} créditos')
     total_creditos += creditos
-print(f'El número total de créditos del curso es {total_creditos}')
-
-
+print(f'El número total de créditos del curso es {total_creditos}')'''
+#ejercicio6
+'''Escribir un programa que cree un diccionario vacío y lo vaya llenado con información sobre una persona (por ejemplo nombre, edad, sexo, teléfono, correo electrónico, etc.) que se le pida al usuario. Cada vez que se añada un nuevo dato debe imprimirse el contenido del diccionario.'''
+'''persona = {}
+while True:
+    clave = input('Qué dato desea guardar? ( ingrese datos de la persona en el formato {calve:valor}, ó "salir" para terminar): ')
+    if clave.lower() == 'salir':
+        break
+    valor = input(f'Introduce el valor para {clave}: ')
+    persona[clave] = valor
+    print(persona)'''
+#ejercicio7
+'''Escribir un programa que cree un diccionario simulando una cesta de la compra. El programa debe preguntar el artículo y su precio y añadir el par al diccionario, hasta que el usuario decida terminar. Después se debe mostrar por pantalla la lista de la compra y el coste total, con el siguiente formato
+Lista de la compra 	
+Artículo 1 	Precio
+Artículo 2 	Precio
+Artículo 3 	Precio
+… 	…
+Total 	Coste'''
+cesta = {}
+while True:
+    articulo = input('Introduce el nombre del artículo y su costo (o "salir" para terminar): ')
+    if articulo.lower() == 'salir':
+        break
+    precio = float(input(f'Introduce el precio de {articulo}: '))
+    cesta[articulo] = precio
+print('Lista de la compra')
+print('Artículo\tPrecio')
+total = 0
+for articulo, precio in cesta.items():
+    print(f'{articulo}\t{precio}')
+    total += precio
+print(f'Total: {total}')
